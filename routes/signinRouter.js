01c296d3e07b80dbaa30528a5bc17a8f3c93f224
Evgenii Rubin <kubikus2009@yandex.ru>
2023-09-05 23:08:17 +0300

    hotfix: ошибки по автотестам

--- routes/signinRouter.js
+++ routes/signinRouter.js
@@ -7,7 +7,7 @@ const {
 
 router.get('/', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().pattern(/^\S+@\S+\.\S+$/),
     password: Joi.string().required().min(2),
   }),
 }), LoginUser);
--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -11,7 +11,8 @@ router.post('/', celebrate({
     about: Joi.string().min(2).max(30),
     // avatar: Joi.string().uri(),
     avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
-    email: Joi.string().required().email(),
+    // email: Joi.string().required().email(),
+    email: Joi.string().required().pattern(/^\S+@\S+\.\S+$/),
     password: Joi.string().required().min(2),
   }).unknown(true),
 }), createUser);
